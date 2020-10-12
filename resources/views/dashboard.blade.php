245968cf96303ea3a4443659f8b4ef31a69a3116
Bostos <nikolast_metal@hotmail.com>
2020-10-12 15:57:10 +0200

    small fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,25 +1,17 @@
 
-
 <x-app-layout>
     <x-slot name="header">
-        
+        <h2>Standardi</h2>
     </x-slot>
     
-    
-          
-
-          
-
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-
-                @foreach($standards as $standard)
-                <a href="{{ route('standard', $standard->id) }}">{{ $standard->name }}</a>
-                @endforeach
-            </div>
-            </div>
-</div>
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+          @foreach($standards as $standard)
+          <a href="{{ route('standard', $standard->id) }}">{{ $standard->name }}</a>
+          @endforeach
+        </div>
+      </div>
+    </div>
     
 </x-app-layout>
-
