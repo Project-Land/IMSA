ae124b40e408fad9f598a92b5afea833beb87278
Bostos <nikolast_metal@hotmail.com>
2020-10-12 12:31:48 +0200

    added Standard model & home controller

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-jet-welcome />
+                @foreach($standards as $standard)
+                <a href="">{{ $standard->name }}</a>
+                @endforeach
             </div>
         </div>
     </div>
