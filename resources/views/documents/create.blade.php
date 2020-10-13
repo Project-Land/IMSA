1ac37ebd2058c2d4a03e9a69e8f11f8c43f0c3af
unknown <amarkovic@projectland.rs>
2020-10-13 10:04:27 +0200

    document.create view

--- resources/views/documents/create.blade.php
+++ resources/views/documents/create.blade.php
@@ -5,11 +5,21 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                 
-            </div>
-        </div>
+    <div class="mx-auto w-50 mt-20 bg-secondary p-10 rounded">
+    <form>
+    <div class="form-group">
+    <label for="dokument_name">Naziv dokumenta:</label>
+    <input type="text" class="form-control" id="document_name" placeholder="Naziv dokumenta" autofocus>
+  </div>
+  <div class="form-group">
+    <label for="document_version">Verzija:</label>
+    <input type="text" class="form-control" id="document_version" placeholder="Verzija">
+  </div>
+  <div class="form-group">
+    <label for="pdf_file_upload">Izaberi PDF Fajl</label>
+    <input type="file" class="form-control-file" id="pdf_file_upload">
+  </div>
+  <button type="submit" class="btn btn-primary">Kreiraj</button>
+</form>
     </div>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,8 +52,9 @@
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
-
+           
                 @include('nav')
+            
             </header>
 
             <!-- Page Content -->
