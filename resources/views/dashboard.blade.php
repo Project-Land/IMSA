e531594ca9bc2c917b5cd32f9f5238598097787c
Bostos <nikolast_metal@hotmail.com>
2020-10-13 09:22:10 +0200

    dashboard layout fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,9 @@
-
 <x-app-layout>
     <x-slot name="header">
         <h2>Standardi</h2>
-        
     </x-slot>
 
     @foreach($standards as $standard)
         <a href="{{ route('standard', $standard->id) }}">{{ $standard->name }}</a>
     @endforeach
-    
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,6 +52,8 @@
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
+
+                @include('nav')
             </header>
 
             <!-- Page Content -->
