69e93a62de4d0597820d99a65fbefc8736a0a81f
unknown <amarkovic@projectland.rs>
2020-10-21 16:44:00 +0200

    report view

--- resources/views/system_processes/internal_check_report/edit.blade.php
+++ resources/views/system_processes/internal_check_report/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Izveštaj sa interne provere ') }} {{session('standard_name')}}
+            {{ __('Izveštaj sa interne provere ') }} 
         </h2>
     </x-slot>
 
