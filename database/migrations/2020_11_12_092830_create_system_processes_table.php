3256a174ea25a0c8bc90f03bc8b0a77c8c77e48b
Bostos <nikolast_metal@hotmail.com>
2020-11-13 10:26:09 +0100

    migration fix

--- database/migrations/2020_11_12_092830_create_system_processes_table.php
+++ database/migrations/2020_11_12_092830_create_system_processes_table.php
@@ -20,20 +20,6 @@ class CreateSystemProcessesTable extends Migration
             $table->integer('display_order')->nullable();
             $table->timestamps();
         });
-
-        DB::table('system_process')->insert(
-            array(
-                ['name' => 'Upravljanje rizikom', 'route' => 'risk-management', 'display_order' => '1'],
-                ['name' => 'Interne provere', 'route' => 'internal-check', 'display_order' => '2'],
-                ['name' => 'Neusaglašenosti i korektivne mere ', 'route' => 'corrective-measures', 'display_order' => '3'],
-                ['name' => 'Obuke ', 'route' => 'trainings', 'display_order' => '4'],
-                ['name' => 'Ciljevi ', 'route' => 'goals', 'display_order' => '5'],
-                ['name' => 'Odobreni isporučioci ', 'route' => 'suppliers', 'display_order' => '6'],
-                ['name' => 'Zainteresovane strane ', 'route' => 'stakeholders', 'display_order' => '7'],
-                ['name' => 'Upravljanje reklamacijama ', 'route' => 'complaints', 'display_order' => '8'],
-                ['name' => 'Preispitivanje sistema menadžmenta ', 'route' => 'management-system-reviews', 'display_order' => '9'],
-            )
-        );
     }
 
     /**
