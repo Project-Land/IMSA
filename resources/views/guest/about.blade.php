cc8b64c9bea64649daa389cf2a8ee3c5f1fa09c1
unknown <amarkovic@projectland.rs>
2020-12-17 10:03:59 +0100

    lang

--- resources/views/guest/about.blade.php
+++ resources/views/guest/about.blade.php
@@ -9,21 +9,20 @@
     <div class="container mt-5 mx-auto text-lg p-2">
 
         <p class="w-75">
-            Aplikacija podrazumeva efikasno i pouzdano rešenje koje je jednostavno za upotrebu i predstavlja neophodan korak ka unapređenju sistema menadžmenta.
-            Predstavlja pouzdan alat za otklanjanje nepotrebnih gubitaka koji se javljaju zbog spore distribucije dokumenata, nepotrebnih troškova štampanja dokumenata, neprecizne kontrole pristupa dokumentima, česte upotrebe nevalidne verzije dokumentacije, nepravovremenog ili neadekvatnog obaveštavanja zaposlenih o svim izmenama u sistemu, i dr.
+        {{ __('Aplikacija podrazumeva efikasno i pouzdano rešenje koje je jednostavno za upotrebu i predstavlja neophodan korak ka unapređenju sistema menadžmenta.Predstavlja pouzdan alat za otklanjanje nepotrebnih gubitaka koji se javljaju zbog spore distribucije dokumenata, nepotrebnih troškova štampanja dokumenata, neprecizne kontrole pristupa dokumentima, česte upotrebe nevalidne verzije dokumentacije, nepravovremenog ili neadekvatnog obaveštavanja zaposlenih o svim izmenama u sistemu, i dr.') }}
         </p>
 
-        Osim opštih, specifični benefiti primene aplikacije su:
+        {{ __('Osim opštih, specifični benefiti primene aplikacije su:') }}
         <ul class="ml-5">
-            <li><i class="fas fa-check"></i> Obezbeđuje ažurnost korišćene dokumentacije</li>
-            <li><i class="fas fa-check"></i> Olakšava planiranje i organizovanje internih provera</li>
-            <li><i class="fas fa-check"></i> Obezbeđuje sistemsko upravljanje rizicima</li>
-            <li><i class="fas fa-check"></i> Obezbeđuje jasno i pregledno praćenje potreba i zahteva zainteresovanih strana</li>
-            <li><i class="fas fa-check"></i> Olakšava praćenje performansi i ocenjivanje isporučilaca</li>
-            <li><i class="fas fa-check"></i> Značajno ubrzava i pojednostavljuje proces preispitivanja od strane rukovodstva</li>
-            <li><i class="fas fa-check"></i> Omogućava centralizovano i sistemsko praćenje ciljeva, kao i stepena njihove realizacije</li>
-            <li><i class="fas fa-check"></i> Olakšava proces upravljanja reklamacijama</li>
-            <li><i class="fas fa-check"></i> Smanjuje rizik od gubljenja/suspenzije sertifikata</li>
+            <li><i class="fas fa-check"></i> {{ __('Obezbeđuje ažurnost korišćene dokumentacije') }}</li>
+            <li><i class="fas fa-check"></i> {{ __('Olakšava planiranje i organizovanje internih provera') }}</li>
+            <li><i class="fas fa-check"></i> {{ __('Obezbeđuje sistemsko upravljanje rizicima') }}</li>
+            <li><i class="fas fa-check"></i> {{ __('Obezbeđuje jasno i pregledno praćenje potreba i zahteva zainteresovanih strana') }}</li>
+            <li><i class="fas fa-check"></i> {{ __('Olakšava praćenje performansi i ocenjivanje isporučilaca') }}</li>
+            <li><i class="fas fa-check"></i> {{ __('Značajno ubrzava i pojednostavljuje proces preispitivanja od strane rukovodstva') }}</li>
+            <li><i class="fas fa-check"></i> {{ __('Omogućava centralizovano i sistemsko praćenje ciljeva, kao i stepena njihove realizacije') }}</li>
+            <li><i class="fas fa-check"></i> {{ __('Olakšava proces upravljanja reklamacijama') }}</li>
+            <li><i class="fas fa-check"></i> {{ __('Smanjuje rizik od gubljenja/suspenzije sertifikata') }}</li>
         </ul>
 
         <div id="i" class="mt-10 mx-auto" >
