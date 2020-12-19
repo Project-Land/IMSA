69727caf85e03d9cd3e2353efaf30fbc8ef90f78
unknown <amarkovic@projectland.rs>
2020-12-19 21:26:30 +0100

    lang

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command(SendEmailToAdmin::class)->cron('*/5 * * * *');
+        $schedule->command(SendEmailToAdmin::class)->cron('* * * * *');
     }
 
     /**
