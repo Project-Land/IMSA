839f9029b4e772272e9b95cf91a6b73a5d507b54
unknown <amarkovic@projectland.rs>
2021-03-16 10:26:28 +0100

    excel

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+         Maatwebsite\Excel\ExcelServiceProvider::class,
        
 
 
@@ -232,6 +233,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'CustomLog' => App\Facades\CustomLog::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
        
 
     ],
